7385768e38ccc79aa8e71c42fecf6b9cac5fc3e5
Potassium-chromate <home7438072@gmail.com>
2025-05-29 20:35:07 +0800

    Initial Commit

---
+++ Core/Src/esp8266-01.c
@@ -0,0 +1,428 @@
+/*
+ * esp8266-01.c
+ *
+ *  Created on: Aug 22, 2021
+ *      Author: Halil Gök
+ */
+
+#include "esp8266-01.h"
+#include "myprintf.h"
+
+extern UART_HandleTypeDef huart2;
+extern UART_HandleTypeDef huart6;
+
+uint16_t ProtocolNameLength;
+uint16_t ClientIDLength;
+uint8_t connect = 0x10, publishCon = 0x30, subscribeCon = 0x82;
+char *protocolName = "MQTT";
+char *clientID = "Halil";
+uint8_t level = 0x04;
+uint16_t keepAlive = 90;
+uint16_t packetID = 0x01;
+uint8_t Qos = 0x00;
+
+void __printf_ctor(ItemUnit *const me, UART_HandleTypeDef *huart) {
+	me->huart = huart;
+
+}
+/*__printf: you can use this function to communicate
+ *  with the device or pc , to receive information.*/
+void __printf(ItemUnit *const me, const char *string) {
+	if (string[0] != '\0') {
+
+		//memset(Esp_buffer, 0, sizeof(Esp_buffer));
+		sprintf(me->buffer, string);
+		HAL_UART_Transmit(me->huart, (uint8_t*) me->buffer, strlen(me->buffer),
+				1000);
+		HAL_Delay(50);
+		memset(me->buffer, 0, sizeof(me->buffer));
+
+		//memset((char*)pc_rx_buffer, 0, sizeof(pc_rx_buffer));
+	}
+}
+
+void TEST(ItemUnit *const me) {
+	__printf(me, "This is a Uart test.\n");
+	HAL_Delay(250);
+}
+void TEST_AT(ItemUnit *esp, ItemUnit *pc) {
+	//it is a test function that confirms that esp communicates hardware with the pc.
+	//UART3 -> ESP UART2 -> PC
+	uint8_t ESPEventCase = 0, stateTry = 0;
+	__printf(pc, "***** ESP8266-01 Test Software *****\n");
+	__printf(pc, "** It can't be learned without making mistakes. **\n");
+
+//	__printf(esp,"AT+RST\r\n");   // At+RST command sent to ESP
+//	__printf(pc,"Resetting \r\n");
+	HAL_Delay(2000);
+	__printf(pc, "AT command has been sent.");
+	while (ESPEventCase == 0) {
+		myprintf("esp->rxBuffer: %s\r\n", esp->rxBuffer);
+		HAL_Delay(500);
+
+		//   __printf(pc,esp->rxBuffer);
+		if (strstr((char*) (esp->rxBuffer), "OK") != NULL) {
+			__printf(pc, "\r\n");
+			__printf(pc, "The module has been accessed.\r\n");
+			ESPEventCase = 1;
+		}
+
+		if (strstr((char*) (esp->rxBuffer), "OK") == NULL && stateTry == 5) {
+			//myprintf(&huart2, "esp->rxBuffer: %s\r\n", esp->rxBuffer);
+			__printf(pc, "\r\nThe module was not found.\r\n");
+			stateTry = 0;
+			HAL_Delay(500);
+		}
+		__printf(pc, ".");
+		stateTry++;
+		clearBuffer(esp);
+	}
+}
+
+/*ESP_Init: This function should be called after testing.
+  ESP_Init() can be called If hardware and software problems are not encountered.*/
+
+void EspInit(ItemUnit *const me, const char *SSID, const char *PASSWORD,
+		ItemUnit *const pc) {
+	uint8_t ESPInitCase = 0;
+	while (ESPInitCase == 0) {
+		__printf(me, "AT\r\n");
+		if (strstr((char*) (me->rxBuffer), "OK") != NULL) {
+
+			ESPInitCase = 1;
+		}
+
+		clearBuffer(me);
+	}
+	while (ESPInitCase == 1) {
+		__printf(me, "AT+CWMODE?\r\n");
+		HAL_Delay(1500);
+
+		if (strstr((char*) (me->rxBuffer), "+CWMODE:1") != NULL) {
+
+			__printf(pc, "The MODE Setting is Correct\n"); //1 'se application modd
+			ESPInitCase = 2;
+		} else {
+
+			__printf(me, "AT+CWMODE=1\r\n");
+			HAL_Delay(700);
+			__printf(pc, "The MODE has been Changed.\n");
+			ESPInitCase = 2;
+		}
+		clearBuffer(me);
+	}
+	while (ESPInitCase == 2)
+	{
+
+		__printf(me, "AT+CWQAP\r\n");
+		HAL_Delay(700);
+		ESPInitCase = 3;
+		clearBuffer(me);
+	}
+	while (ESPInitCase == 3)
+	{
+		sprintf((char*) me->buffer, "AT+CWJAP=\"%s\",\"%s\"\r\n", SSID,
+				PASSWORD);
+		__printf(me, me->buffer);
+		HAL_Delay(1500);
+
+		if (strstr((char*) (me->rxBuffer), "OK") != NULL) {
+			__printf(pc, "network connection has been made\n");
+			ESPInitCase = 4;
+			clearBuffer(me);
+
+		} else {
+			__printf(pc, "network connection is expected\n");
+			HAL_Delay(500);
+
+		}
+
+	}
+
+}
+void connect_Broker(ItemUnit *const me, const char *Ip, const char *Port,
+		ItemUnit *const pc) {
+	uint8_t ESPBrokerInitCase = 0;
+	uint8_t flag = 0x02;   // 02--> unencrypted
+	__printf(pc,"0.0");
+	while (ESPBrokerInitCase == 0) {
+		__printf(me, "AT+CIPCLOSE\r\n");
+
+		HAL_Delay(250);
+		if (strstr((char*) (me->rxBuffer), "OK") != NULL) {
+			ESPBrokerInitCase = 1;
+
+		}
+		clearBuffer(me);
+
+	}
+	while (ESPBrokerInitCase == 1) {
+		__printf(me, "AT+CIPMUX=0\r\n");
+		HAL_Delay(250);
+
+		if (strstr((char*) (me->rxBuffer), "OK") != NULL) {
+			ESPBrokerInitCase = 2;
+			clearBuffer(me);
+		}
+		clearBuffer(me);
+
+	}
+	while (ESPBrokerInitCase == 2) {
+		__printf(me, "AT+CIFSR\r\n");
+		HAL_Delay(1000);
+
+		// It sends error information until it receives an IP. We'll split it up. =)
+		if (strstr((char*) (me->rxBuffer), "ERROR") == NULL)
+
+		{
+			__printf(pc, "Alinan IP = \n"); // 11 Of the incoming information.it says the IP address from the character.
+			__printf(pc, (char*) (me->rxBuffer + 11));
+			ESPBrokerInitCase = 3;
+		}
+       else {
+			__printf(pc, "Try Again.\n");
+
+			}
+		clearBuffer(me);
+	}
+	while (ESPBrokerInitCase == 3) {
+
+		sprintf((char*) me->buffer, "AT+CIPSTART=\"TCP\",\"%s\",%s\r\n", Ip,
+				Port);
+		HAL_UART_Transmit(me->huart, (uint8_t*) me->buffer,
+				sprintf((char*) me->buffer, "AT+CIPSTART=\"TCP\",\"%s\",%s\r\n",
+						Ip, Port), 5000);
+		HAL_Delay(2000);
+
+		__printf(pc, "TCP Connection request has been sent\n\n");
+		HAL_Delay(200);
+		if (strstr((char*) (me->rxBuffer), "OK") != NULL) {
+
+			__printf(pc, "A connection has been established with the site\n\n");
+
+			ESPBrokerInitCase = 4;
+
+		} else {
+			HAL_Delay(1000);
+			__printf(pc, "Try Again.\n");
+
+		}
+
+		clearBuffer(me);
+	}
+	ProtocolNameLength = strlen(protocolName);
+	ClientIDLength = strlen(clientID);
+	uint8_t Remainlength;
+	Remainlength = 2 + ProtocolNameLength + 6 + ClientIDLength;
+	uint16_t length = sprintf((char*) me->buffer, "%c%c%c%c%s%c%c%c%c%c%c%s",
+			(char) connect, (char) Remainlength,
+			(char) (ProtocolNameLength << 8), (char) ProtocolNameLength,
+			protocolName, (char) level, (char) flag, (char) (keepAlive << 8),
+			(char) keepAlive, (char) (ClientIDLength << 8),
+			(char) ClientIDLength, clientID);
+
+	HAL_UART_Transmit(me->huart, (uint8_t*) me->buffer,
+			sprintf((char*) me->buffer, "AT+CIPSEND=%d\r\n", length), 1000);
+	HAL_Delay(100);
+	HAL_UART_Transmit(me->huart, (uint8_t*) me->buffer,
+			sprintf(((char*) me->buffer), "%c%c%c%c%s%c%c%c%c%c%c%s",
+					(char) connect, (char) Remainlength,
+					(char) (ProtocolNameLength << 8), (char) ProtocolNameLength,
+					protocolName, (char) level, (char) flag,
+					(char) (keepAlive << 8), (char) keepAlive,
+					(char) (ClientIDLength << 8), (char) ClientIDLength,
+					clientID), 5000);
+	__printf(pc, "connected to the server..\n");
+
+}
+
+void Connect_Secure_Broker(ItemUnit *const me, const char *Ip, const char *Port,
+		const char *userName, const char *password, ItemUnit *const pc) {
+	uint8_t ESPBrokerInitCase2 = 0;
+	uint16_t usernamelength;
+	uint16_t passwordlength;
+	uint8_t flag = 0xC2;
+	while (ESPBrokerInitCase2 == 0) {
+		__printf(me, "AT+CIPCLOSE\r\n");
+		HAL_Delay(250);
+	//	if (strstr((char*) (me->rxBuffer), "OK") != NULL) {
+			ESPBrokerInitCase2 = 1;
+
+	//	}
+		//clearBuffer(me);
+
+	}
+	while (ESPBrokerInitCase2 == 1) {
+		__printf(me, "AT+CIPMUX=0\r\n");
+		HAL_Delay(250);
+	//	if (strstr((char*) (me->rxBuffer), "OK") != NULL) {
+			ESPBrokerInitCase2 = 2;
+		//	clearBuffer(me);
+	//	}
+
+
+	}
+	while (ESPBrokerInitCase2 == 2) {
+		__printf(me, "AT+CIFSR\r\n");
+		HAL_Delay(700);
+
+
+//	if (strstr((char*) (me->rxBuffer), "ERROR") == NULL)
+		{
+			__printf(pc, "Alinan IP = \n");
+			__printf(pc, (char*) (me->rxBuffer + 11));
+			ESPBrokerInitCase2 = 3;
+//		} else {  // ERROR der ise tekrar dene
+//			HAL_Delay(1000);
+//			__printf(pc, "Tekrar Dene.\n");
+//
+	//}
+		clearBuffer(me);
+	}}
+	while (ESPBrokerInitCase2 == 3) {
+
+		sprintf((char*) me->buffer, "AT+CIPSTART=\"TCP\",\"%s\",%s\r\n", Ip,
+				Port);
+		HAL_UART_Transmit(me->huart, (uint8_t*) me->buffer,
+				sprintf((char*) me->buffer, "AT+CIPSTART=\"TCP\",\"%s\",%s\r\n",
+						Ip, Port), 5000);
+		HAL_Delay(1500);
+
+		__printf(pc, "TCP Connection request has been sent\n\n");
+		HAL_Delay(200);
+		if (strstr((char*) (me->rxBuffer), "OK") != NULL) {
+
+			__printf(pc, "A connection has been established with the site\n\n");
+
+			ESPBrokerInitCase2 = 4;
+
+		} else {
+			HAL_Delay(1000);
+			__printf(pc, "Try Again.\n");
+
+		}
+
+	}
+	ProtocolNameLength = strlen(protocolName);
+	ClientIDLength = strlen(clientID);
+	usernamelength = strlen(userName);
+	passwordlength = strlen(password);
+	uint8_t Remainlength;
+	Remainlength = 2 + ProtocolNameLength + 6 + ClientIDLength + 2
+			+ usernamelength + 2 + passwordlength;
+	uint16_t length = sprintf((char*) me->buffer,
+			"%c%c%c%c%s%c%c%c%c%c%c%s%c%c%s%c%c%s", (char) connect,
+			(char) Remainlength, (char) (ProtocolNameLength << 8),
+			(char) ProtocolNameLength, protocolName, (char) level, (char) flag,
+			(char) (keepAlive << 8), (char) keepAlive,
+			(char) (ClientIDLength << 8), (char) ClientIDLength, clientID,
+			(char) (usernamelength << 8), (char) usernamelength, userName,
+			(char) (passwordlength << 8), (char) passwordlength, password);
+
+	HAL_UART_Transmit(me->huart, (uint8_t*) me->buffer,
+			sprintf((char*) me->buffer, "AT+CIPSEND=%d\r\n", length), 1000);
+	HAL_Delay(250);
+	HAL_UART_Transmit(me->huart, (uint8_t*) me->buffer,
+			sprintf((char*) me->buffer, "%c%c%c%c%s%c%c%c%c%c%c%s%c%c%s%c%c%s",
+					(char) connect, (char) Remainlength,
+					(char) (ProtocolNameLength << 8), (char) ProtocolNameLength,
+					protocolName, (char) level, (char) flag,
+					(char) (keepAlive << 8), (char) keepAlive,
+					(char) (ClientIDLength << 8), (char) ClientIDLength,
+					clientID, (char) (usernamelength << 8),
+					(char) usernamelength, userName,
+					(char) (passwordlength << 8), (char) passwordlength,
+					password), 5000);
+	HAL_Delay(250);
+	__printf(pc, "Connected to the server..\n");
+	clearBuffer(me);
+
+}
+void Subscribe_MQTT(ItemUnit *const me, const char *topic, ItemUnit *const pc) {
+
+	uint16_t TopicLength = strlen(topic);
+	uint8_t RemainLength = 2 + 2 + TopicLength + 1; // packetIDlength(2) + topiclengthdata(2)+topiclength+Qos
+	uint16_t length = sprintf((char*) me->buffer, "%c%c%c%c%c%c%s%c",
+			(char) subscribeCon, (char) RemainLength, (char) (packetID << 8),
+			(char) packetID, (char) (TopicLength << 8), (char) TopicLength,
+			topic, (char) Qos);
+	HAL_UART_Transmit(me->huart, (uint8_t*) me->buffer,
+			sprintf((char*) me->buffer, "AT+CIPSEND=%d\r\n", length), 1000);
+	HAL_Delay(100);
+	HAL_UART_Transmit(me->huart, (uint8_t*) me->buffer,
+			sprintf((char*) me->buffer, "%c%c%c%c%c%c%s%c", (char) subscribeCon,
+					(char) RemainLength, (char) (packetID << 8),
+					(char) packetID, (char) (TopicLength << 8),
+					(char) TopicLength, topic, (char) Qos), 5000);
+	__printf(pc, "'");
+	__printf(pc, topic);
+	__printf(pc, "'");
+	__printf(pc, "subscribed\n");
+	clearBuffer(me);
+
+}
+void publish_MQTT(ItemUnit *const me, char *topic, char *message) {
+	uint16_t topiclength = strlen(topic);
+	uint8_t remainlength = 2 + topiclength + strlen(message);
+	int length = sprintf((char*) me->buffer, "%c%c%c%c%s%s",
+			(char) publishCon, (char) remainlength, (char) (topiclength << 8),
+			(char) topiclength, topic, message);
+	HAL_UART_Transmit(me->huart, (uint8_t*) me->buffer,
+			sprintf((char*) me->buffer, "AT+CIPSEND=%d\r\n", length), 100);
+	HAL_Delay(100);
+	HAL_UART_Transmit(me->huart, (uint8_t*) me->buffer,
+			sprintf((char*) me->buffer, "%c%c%c%c%s%s", (char) publishCon,
+					(char) remainlength, (char) (topiclength << 8),
+					(char) topiclength, topic, message), 5000);
+	HAL_Delay(100);
+}
+void Read_Message_MQTT(ItemUnit *const me, ItemUnit *const pc) {
+	char data_buffer[100];
+	int remain_length = 0, message_length = 0, topic_length = 0;
+	char message[100];
+//	HAL_UART_AbortReceive_IT(me->huart);
+	for (int i = 0; i < sizeof(me->rxBuffer); i++) {
+		if (me->rxBuffer[i] == 0x30) {
+			remain_length = me->rxBuffer[i + 1];
+			topic_length = me->rxBuffer[i + 2] + me->rxBuffer[i + 3];
+			message_length = remain_length - (topic_length + 2);
+			for (int j = 0; j < message_length; j++) {
+
+				message[j] = me->rxBuffer[i + 4 + topic_length + j];
+
+			}
+			break;
+		}
+	}
+
+	for (int a = 0; a < message_length; a++) {
+		data_buffer[a] = message[a];
+	}
+	if (data_buffer[0] != '\0') {
+		__printf(pc, "Message:");
+		__printf(pc, data_buffer);
+		__printf(pc, "\n");
+	}
+
+	if (strcmp(data_buffer, "test mqtt") == 0) {
+		publish_MQTT(me, "**SUBJECT**", "Test successful");
+	}
+
+	if (message[0] == 'O' && message[1] == 'N') {
+		publish_MQTT(me, "**SUBJECT**", "Enable");
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, ENABLE);
+	}
+	if (message[0] == 'O' && message[1] == 'F' && message[2] == 'F') {
+		publish_MQTT(me, "**SUBJECT**", "Disable");
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, DISABLE);
+	}
+	memset(message, 0, sizeof(message));
+	clearBuffer(me);
+	memset(data_buffer, 0, sizeof(data_buffer));
+	//HAL_UART_Receive_IT(&huart3, &byte_esp, 1);
+}
+void clearBuffer(ItemUnit *const me)
+{
+	memset((char*) me->rxBuffer, 0, strlen((char*) me->rxBuffer));
+	me->rxbufferIndex = 0;
+}
---
+++ Core/Src/main.c
@@ -0,0 +1,387 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Esp8266-01 Test-Function
+ * Author          : Halil Gök
+ * websites        :https://embeddedsystemshalilgk.wordpress.com
+ * Youtube         :https://www.youtube.com/channel/UC8cJpAVnScqDzZ7IFs2tQMw
+ ******************************************************************************
+ * @attention
+ *ESP_Init();this function should be called after testing. ESP_Init() can be called
+ *this If hardware and software problems are not encountered.
+ *test_AT();first, you must query with the test_AT command.
+ *  USART2 -> PC
+ *  USART3 ->ESP8266
+ *We do the data retrieval part with interrupt.
+ * Don't forget to activate the interrupt.
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <string.h>
+#include <stdio.h>
+#include "esp8266-01.h"
+#include "myprintf.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+const char* WiFi_Name = "****";
+const char* Wifi_Password = "****";
+const char* Port = "***";
+const char* IP = "***";
+const char* User_Name = "***";
+const char* User_Password = "****";
+
+const char* TOPIC = "***";
+
+
+#define Test_uartPC USART2
+#define Test_uartESP USART3
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+UART_HandleTypeDef huart3;
+UART_HandleTypeDef huart6;
+
+/* USER CODE BEGIN PV */
+ItemUnit Test_uartPc,Test_uartEsp;
+
+
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_USART3_UART_Init(void);
+static void MX_USART6_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+
+
+
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void ESP8266_SendCommand(char *cmd)
+{
+    myprintf(&huart6, "%s\r\n", cmd);
+    //myprintf(&huart2, "%s\r\n", cmd);
+}
+
+void ESP8266_ReadResponse()
+{
+    char resp[128];
+    HAL_UART_Receive(&huart6, (uint8_t*)resp, sizeof(resp), 2000); // Blocking read
+    myprintf(&huart2, "ESP Response: %s\n", resp);
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_USART3_UART_Init();
+  MX_USART6_UART_Init();
+  /* USER CODE BEGIN 2 */
+  ESP8266_SendCommand("AT");           // Check if ESP is alive
+  ESP8266_ReadResponse();
+
+  ESP8266_SendCommand("AT+CWMODE=1");  // Set to Station mode
+  ESP8266_ReadResponse();
+
+  ESP8266_SendCommand("AT+CWJAP=\"AndroidAPF7C1\",\"eason901215\"");  // Connect to WiFi
+  ESP8266_ReadResponse();
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	while (1)
+	{
+
+		Read_Message_MQTT(&Test_uartEsp, &Test_uartPc);
+	   HAL_Delay(50);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	}
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief USART3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART3_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART3_Init 0 */
+
+  /* USER CODE END USART3_Init 0 */
+
+  /* USER CODE BEGIN USART3_Init 1 */
+
+  /* USER CODE END USART3_Init 1 */
+  huart3.Instance = USART3;
+  huart3.Init.BaudRate = 11520;
+  huart3.Init.WordLength = UART_WORDLENGTH_8B;
+  huart3.Init.StopBits = UART_STOPBITS_1;
+  huart3.Init.Parity = UART_PARITY_NONE;
+  huart3.Init.Mode = UART_MODE_TX_RX;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART3_Init 2 */
+
+  /* USER CODE END USART3_Init 2 */
+
+}
+
+/**
+  * @brief USART6 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART6_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART6_Init 0 */
+
+  /* USER CODE END USART6_Init 0 */
+
+  /* USER CODE BEGIN USART6_Init 1 */
+
+  /* USER CODE END USART6_Init 1 */
+  huart6.Instance = USART6;
+  huart6.Init.BaudRate = 115200;
+  huart6.Init.WordLength = UART_WORDLENGTH_8B;
+  huart6.Init.StopBits = UART_STOPBITS_1;
+  huart6.Init.Parity = UART_PARITY_NONE;
+  huart6.Init.Mode = UART_MODE_TX_RX;
+  huart6.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart6.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart6) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART6_Init 2 */
+
+  /* USER CODE END USART6_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, WIFI_RST_Pin|WIFI_EN_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : WIFI_RST_Pin WIFI_EN_Pin */
+  GPIO_InitStruct.Pin = WIFI_RST_Pin|WIFI_EN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+
+/*HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart):
+ * this function is standard uart.C is the function.
+ *  This function is called every time the interrupt enters
+ *   the interrupt.We do 1 byte to 1 byte reading. We'll record
+ *   it in the series. */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+	if (huart->Instance == Test_uartPC) //queried which uart is used
+	{
+		HAL_UART_Receive_IT(Test_uartPc.huart, &Test_uartPc.byteRx, 1);
+
+		Test_uartPc.rxBuffer[Test_uartPc.rxbufferIndex] = Test_uartPc.byteRx;
+		Test_uartPc.rxbufferIndex++;
+
+	}
+
+	if (huart->Instance == Test_uartESP) {
+		HAL_UART_Receive_IT(Test_uartEsp.huart,&Test_uartEsp.byteRx, 1);
+
+		Test_uartEsp.rxBuffer[Test_uartEsp.rxbufferIndex] = Test_uartEsp.byteRx;
+		Test_uartEsp.rxbufferIndex++;
+
+	}
+}
+
+
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
